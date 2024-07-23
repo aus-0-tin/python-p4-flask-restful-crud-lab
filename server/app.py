fdbf646558856bc35e01aa5deedb64be3f494d99
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-07-23 21:11:03 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -16,7 +16,6 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
 
     def get(self):
@@ -24,6 +23,7 @@ class Plants(Resource):
         return make_response(jsonify(plants), 200)
 
     def post(self):
+
         data = request.get_json()
 
         new_plant = Plant(
@@ -37,19 +37,15 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
 
-
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
-
 api.add_resource(PlantByID, '/plants/<int:id>')
-
+        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
